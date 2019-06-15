af83f53a153d91b5f91260519949254a306968a5
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-15 03:04:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function lowerCaseDrivers (drivers){
     return driver.toLowerCase();
   });
 }
+
+function nameToAttributes (drivers){
+  return drivers.map(function (driver){
+    const driverFirstName = driver.split(' ')[0];
+    const driverLastName = driver.split(' ')[1];
+    return {firstName: driverFirstName, lastName: driverLastName};
+  });
+}
